8fcf66677e5bc5859d2339ba7cab30513aa0ddb9
Opuriche <jstcmoses@gmail.com>
2023-12-04 23:52:38 -0800

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,13 +9,13 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *slow = list;
+	listint_t *fast = list;
+
 	if (list == NULL || list->next == NULL)
 	{
 		return (0);
 	}
-
-	listint_t *slow = list;
-	listint_t *fast = list;
 
 	while (fast != NULL && fast->next != NULL)
 	{
